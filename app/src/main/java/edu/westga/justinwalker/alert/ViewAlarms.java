d50a868be850e05be20fdf71e3e1952f69e4bd91
Justin Walker <jtwalker@users.noreply.github.com>
2014-10-20 16:25:19 -0400

    All alarms are now deleted through the options menu in view alarms.

--- app/src/main/java/edu/westga/justinwalker/alert/ViewAlarms.java
+++ app/src/main/java/edu/westga/justinwalker/alert/ViewAlarms.java
@@ -1,14 +1,21 @@
 package edu.westga.justinwalker.alert;
 
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
+import java.util.ArrayList;
+
 import edu.westga.justinwalker.alert.db.AlarmContract.Alarms;
 import edu.westga.justinwalker.alert.db.controller.DBAccess;
+import edu.westga.justinwalker.alert.services.AlarmReceiverActivity;
 
 public class ViewAlarms extends Activity {
 
@@ -50,5 +57,35 @@ public class ViewAlarms extends Activity {
         ListView listView = (ListView) findViewById(R.id.alarmListView);
 
         listView.setAdapter(dataAdapter);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.action_delete_alarms:
+                invokeCommandToDeleteAlarms();
+                Intent intent = getIntent();
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                finish();
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void invokeCommandToDeleteAlarms() {
+        ArrayList<Integer> alarmIDs = this.dbAccess.getAllAlarmIDs();
+
+        for(int id: alarmIDs) {
+            Intent intent = new Intent(this, AlarmReceiverActivity.class);
+            intent.putExtra("requestCode", id);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, id, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+            AlarmManager alarmManager = (AlarmManager) getSystemService(Activity.ALARM_SERVICE);
+            alarmManager.cancel(pendingIntent);
+        }
+
+        this.dbAccess.deleteAllAlarms();
     }
 }
